fab7f67c7be9418ab98591645d68aecd1c203aee
siddharthshiv1201 <shivsiddharth1201@gmail.com>
2025-07-08 11:59:16 +0530

    backend

---
+++ server.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+import express from "express";
+import dotenv from "dotenv";
+import userRouter from "./routes/userRoute.js";
+dotenv.config();
+
+const app = express();
+const dbuser = encodeURIComponent(process.env.DBUSER);
+const dbpass = encodeURIComponent(process.env.DBPASS);
+
+// mongoose
+//   .connect(`mongodb://${dbuser}:${dbpass}@localhost:27017/lpu?authsource=admin`)
+//   .then(() => {
+//     app.listen(8080, () => {
+//       console.log("Server started");
+//     });
+//   });
+
+  mongoose
+  .connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.mz2ddbv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+  .then(() => {
+    app.listen(8080, () => {
+      console.log("Server started");
+    });
+  });
+
+app.use(express.json());
+
+app.use("/api/users", userRouter);
